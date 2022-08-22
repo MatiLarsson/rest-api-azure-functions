4d7982ec29d3c198accd3dcdb674d1f72d8e7ec8
Matias Larsson <larssonmts@gmail.com>
2022-08-22 16:49:38 -0300

    finished api

---
+++ middleware/validator.js
@@ -0,0 +1,19 @@
+exports.validateBody = async (context, body, schema) => {
+  try {
+    if (!body) {
+      context.res = {
+        status: 400,
+        body: "Request body is empty"
+      }
+      context.done()
+      return
+    }
+    await schema.validateAsync(body)
+  } catch (error) {
+    context.res = {
+      status: 400,
+      body: error.message
+    }
+    context.done()
+  }
+}
